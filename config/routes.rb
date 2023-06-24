41f33f4c1fb07df9df052a3889258ededa904b47
ayakuma309 <ayako.30901@gmail.com>
2023-06-24 18:52:26 +0900

    feat: bookのルーティング #10 #11

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :books, param: :isbn do
+    collection do
+      get :search
+    end
+  end
 end
