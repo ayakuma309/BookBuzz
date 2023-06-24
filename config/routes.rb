8011ec41bbbad193088bacd18a090dabe7753fa1
ayakuma309 <ayako.30901@gmail.com>
2023-06-24 14:29:23 +0900

    feat: ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
   resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
