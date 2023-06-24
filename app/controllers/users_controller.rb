e7d47694180d6dfcacb4a24137c5bd6c1c0a37f6
ayakuma309 <ayako.30901@gmail.com>
2023-06-24 14:29:14 +0900

    feat:　新規登録 #9

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	skip_before_action :require_login
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path, success: t('.success')
+    else
+			flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
+end
