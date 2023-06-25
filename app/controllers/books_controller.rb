e5082f80bbd2ec768cd7112f01ef7bf0e9f6fede
ayakuma309 <ayako.30901@gmail.com>
2023-06-25 18:56:26 +0900

    style: topページのコンテンツはログインしてなくても利用できるようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_login, only: %i[index  genres_search]
 
   def index
     @books = Book.order('RANDOM()').limit(3)
