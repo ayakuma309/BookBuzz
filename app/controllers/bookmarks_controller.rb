a80a46f927b4e8017ebc6fc4e6e15271fb966444
ayakuma309 <ayako.30901@gmail.com>
2023-06-26 21:41:23 +0900

    feat: ブックマークコントローラー修正 #22

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,18 +1,12 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmark_books = current_user.bookmarks.includes(:book)
-  end
-
   def create
     @book = Book.find_by(isbn: params[:book_isbn])
     current_user.bookmarks.create!(book: @book)
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find_by(isbn: params[:book_isbn])
     bookmark = current_user.bookmarks.find_by!(book: @book)
     bookmark.destroy!
-    redirect_to request.referer
   end
 end
