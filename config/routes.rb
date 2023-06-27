47c321ff7ec9c90df5f14f6f9760a008600da2b3
ayakuma309 <ayako.30901@gmail.com>
2023-06-27 11:37:27 +0900

    feat: admin用のルーティング設定 #30

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,11 @@ Rails.application.routes.draw do
     end
     resource :bookmark, only: %i[create destroy]
   end
+
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
