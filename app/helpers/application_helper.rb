a4c6797c2f03bc32702f8022f03a46ad40bc9772
ayakuma309 <ayako.30901@gmail.com>
2023-06-27 12:59:43 +0900

    feat: OGP設定 #20

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,31 @@ module ApplicationHelper
                 end
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'マスクをした本たち',
+      title: 'マスクをした本たち',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'いつもとは違った方法で本を選ぶサービス',
+      keywords: 'マスクをした本たち, 本の宝さがし, おすすめの本',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@ZCunkuma',
+        image: image_url('ogp.png'),
+      }
+    }
+  end
 end
