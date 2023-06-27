f5fdcae4c44d93fbaa71b5b7cd3bdb96ab1667fb
ayakuma309 <ayako.30901@gmail.com>
2023-06-27 22:44:11 +0900

    feat: ルーティング#35

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
 
-    resources :books, param: :isbn, only: %i[index destroy]
+    resources :books, param: :isbn, only: %i[index edit update destroy]
+    resources :tags, only: [:index, :create, :edit, :update, :destroy]
   end
 end
