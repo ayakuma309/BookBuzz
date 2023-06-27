083c15ed0776c486dbe638839c1e767960ac62c0
ayakuma309 <ayako.30901@gmail.com>
2023-06-27 10:51:27 +0900

    feat: マイページ作成(退会機能も追加) #12

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'static_pages#top'
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
-  resources :users, only: %i[new create]
+  resources :users, only: %i[show new create destroy]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
