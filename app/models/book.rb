00db96810595ceebaabc6bfbc4727d87d88956f0
ayakuma309 <ayako.30901@gmail.com>
2023-06-27 22:46:16 +0900

    feat: model同士の紐付け #35

--- app/models/book.rb
+++ app/models/book.rb
@@ -16,8 +16,20 @@ class Book < ApplicationRecord
   self.primary_key = :isbn
 
   has_many :bookmarks, foreign_key: 'book_isbn', primary_key: 'isbn', dependent: :destroy
+  has_many :book_tags, foreign_key: 'book_isbn', dependent: :destroy
+  has_many :tags, through: :book_tags
 
   validates :isbn, uniqueness: { scope: :isbn }
+
+  def save_with(tag_ids)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_ids.map { |id| Tag.find(id) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
 
   def self.generate_twitter_links(books)
     encoded_share_text = books.map { |book| "#{book.author} : #{book.title}%0a" }
