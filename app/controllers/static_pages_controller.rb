a1a47b696574d1effda02f9dc95fbcf3840ea0ac
ayakuma309 <ayako.30901@gmail.com>
2023-07-06 14:55:27 +0900

    refactor: マジックナンバーを修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
+
+  MAX_GENRES = 6
+  
   def top; end
 
   def recommend_genre
     response = RakutenWebService::Books::Genre.search(booksGenreId: Rails.configuration.bookGenreId)
-    @genres = response.first.children.take(6)
+    @genres = response.first.children.take(MAX_GENRES)
   end
 
   def terms; end
