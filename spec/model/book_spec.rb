9ffc5f6e0105af2781d895ad6e351a3c85cdc5fa
ayakuma309 <ayako.30901@gmail.com>
2023-07-30 14:49:09 +0900

    add: bookモデル, おすすめロジックのtest

--- spec/model/book_spec.rb
+++ spec/model/book_spec.rb
@@ -49,21 +49,122 @@ RSpec.describe Book, type: :model do
           expect(book.save_with(tag_ids)).to be_truthy
         end
       end
-      context 'タグが保存されないとき' do
-        it 'アソシエーションのテスト' do
-          expect {
-            book.save_with([tag1.id, 999]) # 999 is an invalid tag id
-          }.to raise_error(ActiveRecord::RecordInvalid)
-            .and not_change { BookTag.count }
-            .and not_change { book.tags.count }
-        end
+    end
 
+    describe '#bookmark?' do
+      let(:user) { create(:user) }
+      let(:book) { create(:book) }
+
+      context 'ブックマークされたとき' do
+        before { create(:bookmark, user: user, book: book) }
+
+        it 'returns true' do
+          expect(book.bookmark?(user)).to be_truthy
+        end
+      end
+
+      context 'ブックマーク外したとき' do
         it 'returns false' do
-          expect(book.save_with([tag1.id, 999])).to be_falsey
+          expect(book.bookmark?(user)).to be_falsey
         end
       end
     end
 
-    
+    describe 'recommend_booksメソッド' do
+      let(:user) { create(:user) }
+      let!(:book1) { create(:book) }
+      let!(:book2) { create(:book, isbn: '56789') }
+      let!(:book3) { create(:book, isbn: '67890') }
+
+      before do
+        create(:bookmark, user: user, book: book1)
+        create(:bookmark, user: user, book: book2)
+      end
+
+      it 'ユーザーがブックマークした本のタグを分析し、同じ種類のタグを持つ本を推薦するメソッド' do
+        expect(Book).to receive(:get_bookmarked_books).with(user).and_return([book1, book2])
+        expect(Book).to receive(:get_bookmarked_tags).with([book1, book2]).and_return(['tag1', 'tag2'])
+        expect(Book).to receive(:find_similar_books).with([book1, book2], ['tag1', 'tag2']).and_return([book3])
+        expect(Book).to receive(:get_top_recommendations).with([book3]).and_return([book3])
+
+        recommended_books = Book.recommend_books(user)
+        expect(recommended_books).to eq([book3])
+      end
+    end
+
+    describe 'get_bookmarked_booksメソッド' do
+      let(:user) { create(:user) }
+      let!(:book1) { create(:book) }
+      let!(:book2) { create(:book, isbn: '56789') }
+
+      before { create(:bookmark, user: user, book: book1) }
+
+      it 'ユーザーがブックマークした本の配列を取得' do
+        expect(Book.get_bookmarked_books(user)).to eq([book1])
+      end
+    end
+
+    describe 'get_bookmarked_tagsメソッド' do
+      let!(:book1) { create(:book) }
+      let!(:book2) { create(:book, isbn: '56789') }
+      let!(:tag1) { create(:tag) }
+      let!(:tag2) { create(:tag) }
+
+      before do
+        book1.tags << tag1
+        book2.tags << tag2
+      end
+
+      it 'ブックマークした本のタグの収集（重複を除く）' do
+        bookmarked_books = [book1, book2]
+        expect(Book.get_bookmarked_tags(bookmarked_books)).to eq([tag1, tag2])
+      end
+    end
+
+    describe 'calculate_similarityメソッド' do
+      let!(:tag1) { create(:tag) }
+      let!(:tag2) { create(:tag) }
+      let!(:tag3) { create(:tag) }
+
+      it 'タグの類似度を計算するメソッド' do
+        bookmarked_tags = [tag1, tag2]
+        book_tags = [tag2, tag3]
+        similarity = Book.calculate_similarity(bookmarked_tags, book_tags)
+        expect(similarity).to be_within(0.01).of(0.33)
+      end
+
+      it 'タグリストが空の場合は類似度を0とする' do
+        bookmarked_tags = []
+        book_tags = [tag1, tag2]
+        similarity = Book.calculate_similarity(bookmarked_tags, book_tags)
+        expect(similarity).to eq(0)
+
+        bookmarked_tags = [tag1, tag2]
+        book_tags = []
+        similarity = Book.calculate_similarity(bookmarked_tags, book_tags)
+        expect(similarity).to eq(0)
+      end
+    end
+
+    describe 'recommend_books_with_different_tagsメソッド' do
+      let(:user) { create(:user) }
+      let!(:book1) { create(:book) }
+      let!(:book2) { create(:book, isbn: '56789') }
+      let!(:book3) { create(:book, isbn: '67890') }
+      let!(:tag1) { create(:tag) }
+      let!(:tag2) { create(:tag) }
+      let!(:tag3) { create(:tag) }
+
+      before do
+        create(:bookmark, user: user, book: book1)
+        book1.tags << tag1
+        book2.tags << tag2
+        book3.tags << tag3
+      end
+
+      it 'ユーザーがブックマークした本のタグを分析し、違う種類のタグを持つ本を5件推薦するメソッド' do
+        expect(Book.recommend_books_with_different_tags(user).count).to eq(2)
+      end
+    end
   end
 end
